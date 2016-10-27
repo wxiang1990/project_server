29b24eac2f2042eb5e4bfc1a95b00a24cc866e13
wxiang1990 <wxiang1990@126.com>
2016-10-27 11:12:25 +0800

    add struct of tcp header.

--- gloabaldefine.h
+++ gloabaldefine.h
@@ -1,4 +1,12 @@
 #ifndef _GLOBALDEFINE_H_
 #define _GLOBALDEFINE_H_
 
+typedef stuct _stu_tcp_header_
+{
+  long data_header;
+  long data_num;
+  long data_serialNo;
+  long command;
+}STU_TCP_HEADER;
+
 #endif
